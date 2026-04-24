d62c97e1c7081ab7f5c83ca79074bec2dcde778e
Yuvraj Singh <ys792608@gmail.com>
2026-04-24 21:26:14 +0530

    Implement bubble sort algorithm in Java

---
+++ Sorting Algorithim/bubbleSorting.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class bubbleSorting {
+    public static  void bubble_sorting(int array[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i =0; i<array.length-1; i++){
+            for(int j=0; j<array.length-1-i; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+
+                }
+            }
+        }
+    }
+
+    public static void printArray(int array[]){
+        for(int i =0; i<array.length; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int array[] ={6, 4, 9, 7, 2,};
+        bubble_sorting(array);
+        printArray(array);
+    }
+}
+
